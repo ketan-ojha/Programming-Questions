bd5266cb0e405da1c5455d7c11d9cf2edc490861
Ketan Ojha <43106807+ketan-ojha@users.noreply.github.com>
2018-10-08 18:37:41 +0530

    added Project Euler #1: Multiples of 3 and 5.c

--- Project Euler #1: Multiples of 3 and 5.c
+++ Project Euler #1: Multiples of 3 and 5.c
@@ -13,9 +13,9 @@ int main(){
         
         scanf("%lld",&n);
         long long int a=0,b=0,c=0,ans=0;
-        a=(n-1)/3;
-        b=(n-1)/5;
-        c=(n-1)/15;
+        a=(n-1)/3;//mul of 3
+        b=(n-1)/5;//mul of 5
+        c=(n-1)/15;//sub mul of 15
         ans=(3*a*(a+1)/2)+(5*b*(b+1)/2)-(15*c*(c+1)/2);
 		printf("%lld\n",ans);	
     }
